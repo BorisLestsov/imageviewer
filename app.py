3a103148bcd1dd505e513d9058ff2e5f575cbbc5
boris <bor1996@mail.ru>
2018-04-11 15:20:17 +0300

    kek

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import csv
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import requests
 from pager import Pager
+import time
 
 
 def read_table(url):
@@ -11,9 +12,10 @@ def read_table(url):
         return [row for row in csv.DictReader(f.readlines())]
 
 
-APPNAME = "PrettyGalaxies"
+APPNAME = "NeuralValidator_v0.0"
 STATIC_FOLDER = 'example'
 TABLE_FILE = "example/fakecatalog.csv"
+f=open('out.txt', 'a+')
 
 table = read_table(TABLE_FILE)
 pager = Pager(len(table))
@@ -30,8 +32,14 @@ def index():
     return redirect('/0')
 
 
-@app.route('/<int:ind>/')
+@app.route('/<int:ind>/', methods=['GET', 'POST'])
 def image_view(ind=None):
+    if request.method == 'POST':
+        text = request.form['text']
+        f.write("mypost: {} {}\n".format(ind, text))
+        f.flush()
+        return redirect('/{}'.format(ind+1))
+
     if ind >= pager.count:
         return render_template("404.html"), 404
     else:
@@ -41,6 +49,8 @@ def image_view(ind=None):
             index=ind,
             pager=pager,
             data=table[ind])
+
+   
 
 
 @app.route('/goto', methods=['POST', 'GET'])    
